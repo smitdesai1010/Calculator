f4ad341054770510abf2367ff368871f4eed9eb1
Smit Desai <57582187+smitdesai1010@users.noreply.github.com>
2020-05-04 01:27:21 -0400

    Added js

---
+++ ToDo App/app.js
@@ -0,0 +1,9 @@
+var list=document.querySelector('.todo-list');
+
+console.log("linked");
+
+document.querySelector('#btn').addEventListener('click',addtolist);
+
+function addtolist(){
+  alert("Done");
+}
