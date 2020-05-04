cdce2ee19522cf720164705e0b71892aa4b1dc1d
Smit Desai <57582187+smitdesai1010@users.noreply.github.com>
2020-05-04 01:54:36 -0400

    Add on click added

--- ToDo App/app.js
+++ ToDo App/app.js
@@ -1,9 +1,28 @@
 var list=document.querySelector('.todo-list');
 
-console.log("linked");
-
 document.querySelector('#btn').addEventListener('click',addtolist);
 
 function addtolist(){
-  alert("Done");
+
+  event.preventDefault();
+
+  console.log("linked");
+  var obj=document.createElement('div');
+  obj.classList.add("list-obj");
+
+  var value=document.createElement('li');
+  value.classList.add("list-obj-value");
+
+  var mark=document.createElement('button');
+  mark.classList.add("list-obj-mark");
+
+  var trash=document.createElement('button');
+  trash.classList.add("list-obj-trash");
+
+   obj.appendChild(value);
+   obj.appendChild(mark);
+   obj.appendChild(trash);
+
+  list.appendChild(obj);
+
 }
