83337fd4bd393891cab3be672b3c677524d4d767
Smit Desai <57582187+smitdesai1010@users.noreply.github.com>
2020-05-04 21:21:50 -0400

    comit

--- ToDo App/app.js
+++ ToDo App/app.js
@@ -1,5 +1,4 @@
 var list=document.querySelector('.todo-list');
-
 document.querySelector('#btn').addEventListener('click',addtolist);
 
 
@@ -12,18 +11,35 @@ function addtolist(){
 
   const value=document.createElement('li');
   value.classList.add("list-obj-value");
+  value.innerText=getvalue();
 
   const mark=document.createElement('button');
   mark.innerHTML='<i class="fas fa-check-square"></i>';
   mark.classList.add("list-obj-mark");
+  mark.setAttribute("onclick" ,'checked(this.previousSibling)');
 
   const trash=document.createElement('button');
   trash.innerHTML='<i class="fas fa-trash"></i>';
   trash.classList.add("list-obj-trash");
+  trash.setAttribute("onclick" ,'trash(this)');
 
    obj.appendChild(value);
    obj.appendChild(mark);
    obj.appendChild(trash);
 
   list.appendChild(obj);
+}
+
+function getvalue(){
+
+  var value=document.querySelector('#input-value').value;
+  document.querySelector('#input-value').value='';
+
+  return value;
+}
+
+
+function checked(doc){
+  doc.style.textDecorationLine="line-through";
+  doc.style.color="red";
 }
