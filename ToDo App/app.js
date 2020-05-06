5b050bdf1fa99bcfc8e0e43f6ca00e369eae82c9
Smit Desai <57582187+smitdesai1010@users.noreply.github.com>
2020-05-05 20:29:22 -0400

    fixed event bubbling

--- ToDo App/app.js
+++ ToDo App/app.js
@@ -7,21 +7,21 @@ function addtolist(){
   event.preventDefault();
 
   const obj=document.createElement('div');
-  obj.classList.add("list-obj");
+  obj.classList.add('list-obj');
 
   const value=document.createElement('li');
-  value.classList.add("list-obj-value");
+  value.classList.add('list-obj-value');
   value.innerText=getvalue();
 
   const mark=document.createElement('button');
   mark.innerHTML='<i class="fas fa-check-square"></i>';
-  mark.classList.add("list-obj-mark");
-  mark.setAttribute("onclick" ,'checked(this.previousSibling)');
+  mark.classList.add('list-obj-mark');
+  mark.setAttribute('onclick' ,'checked(this.previousSibling)');
 
   const trash=document.createElement('button');
   trash.innerHTML='<i class="fas fa-trash"></i>';
-  trash.classList.add("list-obj-trash");
-  trash.setAttribute("onclick" ,'trash(this)');
+  trash.classList.add('list-obj-trash');
+  trash.setAttribute('onclick' ,'trash(this.parentNode)');
 
    obj.appendChild(value);
    obj.appendChild(mark);
@@ -40,6 +40,24 @@ function getvalue(){
 
 
 function checked(doc){
-  doc.style.textDecorationLine="line-through";
-  doc.style.color="red";
+  console.log(doc);
+  doc.classList.toggle('checked');
+}
+
+
+
+function trash(element){
+  console.log(element);
+  element.classList.toggle('delete-animation');
+
+  //Stopped Event bubbling by event capturing 'transitionend' on parent element
+  //set 'transitionend' to not to propgate upwards on parentNode. Then used
+  //event capturing on that so to pass that feature on all childNodes
+  //otherwise it was event bubbling was taking place.
+  element.addEventListener('transitionend',()=>{ event.stopPropagation(); },true);
+
+  element.addEventListener('transitionend',()=>{
+    element.remove();
+  });
+
 }
