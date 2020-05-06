d0bd849791c201911b125cfa5c221186aedd9eb6
Smit Desai <57582187+smitdesai1010@users.noreply.github.com>
2020-05-05 23:42:30 -0400

    Added localStorage

--- ToDo App/app.js
+++ ToDo App/app.js
@@ -1,8 +1,16 @@
 var list=document.querySelector('.todo-list');
-document.querySelector('#btn').addEventListener('click',addtolist);
+
+document.querySelector('#btn').addEventListener('click',()=>{
+  addtolist(getvalue());
+});
+
+document.addEventListener('DOMContentLoaded',()=>{
+  console.log('Loaded');
+  loadtodo();
+})
 
 
-function addtolist(){
+function addtolist(todoobj){
 
   event.preventDefault();
 
@@ -11,7 +19,7 @@ function addtolist(){
 
   const value=document.createElement('li');
   value.classList.add('list-obj-value');
-  value.innerText=getvalue();
+  value.innerText=todoobj;
 
   const mark=document.createElement('button');
   mark.innerHTML='<i class="fas fa-check-square"></i>';
@@ -27,8 +35,13 @@ function addtolist(){
    obj.appendChild(mark);
    obj.appendChild(trash);
 
+  todoList=JSON.parse(localStorage.getItem('todoObjs'));
+  todoList.push(todoobj);
+  localStorage.setItem('todoObjs',JSON.stringify(todoList));
+
   list.appendChild(obj);
 }
+
 
 function getvalue(){
 
@@ -56,8 +69,36 @@ function trash(element){
   //otherwise it was event bubbling was taking place.
   element.addEventListener('transitionend',()=>{ event.stopPropagation(); },true);
 
+  todoList=JSON.parse(localStorage.getItem('todoObjs'));
+
+    for (i=0; i<todoList.length; ++i)
+     if (todoList[i]===element.firstChild.innerHTML)
+      todoList.splice(i,1);
+      
+  localStorage.setItem('todoObjs',JSON.stringify(todoList));
+
   element.addEventListener('transitionend',()=>{
     element.remove();
   });
 
+}
+
+//localStorage.clear();
+
+function loadtodo(){
+
+  var todoList;
+
+  if (localStorage.getItem('todoObjs')===null)
+   todoList=[];
+
+  else
+   todoList=JSON.parse(localStorage.getItem('todoObjs'));
+
+  for (i=0; i<todoList.length; ++i)
+   addtolist(todoList[i]);
+
+//   console.log(typeof todoList);
+
+  localStorage.setItem('todoObjs',JSON.stringify(todoList));
 }
